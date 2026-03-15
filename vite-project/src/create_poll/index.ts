27de8f240e26d38b298b33f994d5e4cfc132f4d3
Dataram57 <contact@dataram57.com>
2026-03-15 18:45:58 +0100

    error checks for create_poll

--- vite-project/src/create_poll/index.ts
+++ vite-project/src/create_poll/index.ts
@@ -1,6 +1,7 @@
 
 
 const p : bigint = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
+const maxParticipants : bigint = 2n ** 8n;
 
 //pattern_participant
 
@@ -8,17 +9,54 @@ function Participant_remove_click(e : Event){
     let tag = e.target as HTMLElement;
     while(tag.tagName.toLowerCase() != "tr")
         tag = tag.parentElement as HTMLElement;
-    tag.remove();
+
+    //remove also useless tbodies.
+    let tag_new: HTMLElement;
+    while(tag.tagName.toLowerCase() != "table"){
+        tag_new = tag.parentElement as HTMLElement;
+        tag.remove();
+        tag = tag_new;
+    }
+}
+
+function Participant_input(e : Event){
+    const input : HTMLInputElement = e.target as HTMLInputElement;
+    const wasError : boolean = input.classList.contains("error");
+    let isError : boolean = false;
+    if(input.value.length != 0){        
+        try{
+            const secret : bigint= BigInt(input.value);
+            if(!(secret >= 0 && secret < p))
+                isError = true;
+        }
+        catch(error : any){
+            isError = true;
+        }
+    }
+    
+    //check display
+    if(isError != wasError)
+        if(isError)
+            input.classList.add("error");
+        else
+            input.classList.remove("error");
 }
 
 function AddNewParticipant(){
     let tag : HTMLElement;
     tag = document.getElementById("participants") as HTMLElement;
+    
+    //block if too many children
+    if(tag.children.length - 1 >= maxParticipants)
+        return;
+
+    //
     tag.insertAdjacentHTML("beforeend", document.getElementById("pattern-participant")?.innerText as string);
 
     //public key
     tag = document.getElementById("pattern-participant-public_key") as HTMLElement;
     tag.id = "";
+    tag.addEventListener("input", Participant_input);
 
     //remove
     tag = document.getElementById("pattern-participant-remove") as HTMLElement;
@@ -32,11 +70,15 @@ function AddN_NewPariticipants(){
         AddNewParticipant();
 }
 
+
+
 export function init() {
     //pariticipants - buttons
     document.getElementById("button-add_paricipant")?.addEventListener("click", AddNewParticipant);
     document.getElementById("button-add_n_pariticipants")?.addEventListener("click", AddN_NewPariticipants);
     AddN_NewPariticipants();
+
+    //
     
 }
 
