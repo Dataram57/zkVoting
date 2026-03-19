90c593938bdb9fb175983894aa28adb9353b24c4
Dataram57 <contact@dataram57.com>
2026-03-19 12:54:16 +0100

    again.

--- server/api/_lib/zk.js
+++ server/api/_lib/zk.js
@@ -1,9 +1,26 @@
 import * as snarkjs from "snarkjs";
-import vote_verifier from "../../vote_verifier.json" assert { type: "json" };
+
+//================================================================
+//import vote_verifier from "../../vote_verifier.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+
+let cachedVerifier;
+
+export function getVerifier() {
+    if (!cachedVerifier) {
+        const verifierPath = path.join(process.cwd(), "api", "vote_verifier.json");
+        cachedVerifier = JSON.parse(fs.readFileSync(verifierPath, "utf-8"));
+    }
+    return cachedVerifier;
+}
+//================================================================
 
 export const verifyProof = async (publicSignals, proof) => {
-    return snarkjs.groth16.verify(vote_verifier, publicSignals, proof);
+  const verifier = getVerifier();
+  return snarkjs.groth16.verify(verifier, publicSignals, proof);
 };
+
 
 export const p =
     21888242871839275222246405745257275088548364400416034343698204186575808495617n;
