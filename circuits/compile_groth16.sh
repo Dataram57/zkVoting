ea944641d23bb443ebfbe0cb422ad3ce9a49fd54
Dataram57 <contact@dataram57.com>
2026-03-21 18:54:45 +0100

    readme + a bit more on setting up the project

--- circuits/compile_groth16.sh
+++ circuits/compile_groth16.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 circom vote.circom --r1cs --sym --wasm
-snarkjs groth16 setup vote.r1cs ceremony_final.ptau vote_prover.zkey
+snarkjs groth16 setup vote.r1cs ppot_0080_14.ptau vote_prover.zkey
 snarkjs zkey export verificationkey vote_prover.zkey vote_verifier.json
