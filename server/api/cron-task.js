79ae30e36c9c290c0cf214782b1505145b520e23
Dataram57 <contact@dataram57.com>
2026-03-22 10:23:00 +0100

    alright it works now

--- server/api/cron-task.js
+++ server/api/cron-task.js
@@ -2,17 +2,12 @@ import { sql } from "./_lib/db.js";
 
 export default async function handler(request, response) {
     const authHeader = request.headers.authorization;
-    //;;
-    console.log(request.headers);
-    console.log(process.env.CRON_SECRET);
     if (
         !process.env.CRON_SECRET ||
         authHeader !== `Bearer ${process.env.CRON_SECRET}`
     ) {
         return response.status(401).json({ success: false });
     }
-
-    console.log("test");
 
     // delete polls older than 3 days
     await sql`
