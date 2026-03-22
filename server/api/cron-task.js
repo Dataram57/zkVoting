ea138c4168fa25fce6ac57ea11864d901e575f42
Dataram57 <contact@dataram57.com>
2026-03-22 10:12:21 +0100

    ... again... I hope it's gonna work

--- server/api/cron-task.js
+++ server/api/cron-task.js
@@ -1,20 +1,22 @@
 import { sql } from "./_lib/db.js";
 
 export default async function handler(request, response) {
-    const authHeader = request.headers.get('authorization');
+    const authHeader = request.headers.authorization;
+
     if (
         !process.env.CRON_SECRET ||
         authHeader !== `Bearer ${process.env.CRON_SECRET}`
     ) {
         return response.status(401).json({ success: false });
     }
+
     console.log("test");
-    //delete polls older than 3 days
+
+    // delete polls older than 3 days
     await sql`
         DELETE FROM polls
         WHERE created_at < now() - interval '3 days';
     `;
 
-    //end
     response.status(200).json({ message: "Task executed" });
 }
