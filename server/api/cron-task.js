523c7e362bc0b9fed25638f4ae8f238e309f794d
Dataram57 <contact@dataram57.com>
2026-03-22 09:59:46 +0100

    again.

--- server/api/cron-task.js
+++ server/api/cron-task.js
@@ -2,7 +2,7 @@ import { sql } from "./_lib/db.js";
 
 export default async function handler(req, res) {
     // Check secret token
-    const authHeader = req.headers.get('authorization');
+    const authHeader = req.headers['authorization'];
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`)
         return new Response('Unauthorized', {
             status: 401,
